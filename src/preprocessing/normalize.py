a054d6ed1f8ea19676af3b1a80720fabf808d40a
Nithin Varughese <153134916+Nithinvarughese@users.noreply.github.com>
2025-11-11 18:33:46 -0500

    Remove print statements in normalize.py
    Removed print statements for normalized lines from files.

--- src/preprocessing/normalize.py
+++ src/preprocessing/normalize.py
@@ -14,11 +14,3 @@ def preprocess_lines(lines):
 def preprocess_file(path):
         raw = read_file(path)
         return preprocess_lines(raw)
-lines1 = preprocess_file("dataset/file_v1.py")
-lines2 = preprocess_file("dataset/file_v2.py")
-print("Normalized lines from file1_v1.py:")
-for i, line in enumerate(lines1, start=1):
-        print(f"{i}: {line}")
-print("\nNormalized lines from file1_v2.py:")
-for i, line in enumerate(lines2, start=1):
-        print(f"{i}: {line}")

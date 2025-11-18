a1bba12d36de76ad618e9d448a96e233d55e7a8b
Sunny Johnson <sunnyjohnsonk7@gmail.com>
2025-11-17 20:00:04 -0500

    matching public API

--- src/matching/__init__.py
+++ src/matching/__init__.py
@@ -1,3 +1,2 @@
-""" starter code"""
 from .similarity import norm_levenshtein, context_cosine, get_context
-from .matcher import match_lines, Matcher, MatchResult
+from .matcher import Matcher, MatchResult, match_lines
